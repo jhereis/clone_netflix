a871767bf3e25ed17f3d61afe77c8c6dbd283c5d
jhenn reis <101225404+jhereis@users.noreply.github.com>
2023-01-03 02:23:09 +0100

    to commit node_modules

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "649e53f7eb768b599b6fc6f387382eae"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
